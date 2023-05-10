91b756aba51d32a89f1483c5d5c50f21ec06343c
Eduardo Frederico <eduardo.frederico@icloud.com>
2023-05-09 22:59:36 -0300

    Initial Commit

---
+++ SwiftQuiz/SwiftQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftQuizApp.swift
+//  SwiftQuiz
+//
+//  Created by Eduardo Frederico on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
