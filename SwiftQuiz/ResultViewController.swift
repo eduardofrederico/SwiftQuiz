9ec698f8b545e7c0decd8663b5782188640582e0
Eduardo Frederico <eduardo.frederico@icloud.com>
2023-06-14 23:23:50 -0300

    A construção do app foi concluída nesta etapa.

--- SwiftQuiz/ResultViewController.swift
+++ SwiftQuiz/ResultViewController.swift
@@ -11,20 +11,31 @@ class ResultViewController: UIViewController {
 
     
     @IBOutlet weak var lbAnswered: UILabel!
-    
     @IBOutlet weak var lbCorrect: UILabel!
-    
     @IBOutlet weak var lbWrong: UILabel!
-    
     @IBOutlet weak var lbScore: UILabel!
+    
+    var totalCorrectAnswers: Int = 0
+    var totalAnswers: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        lbAnswered.text = "Perguntas Respondidas: \(totalAnswers)"
+        lbCorrect.text = "Perguntas Corretas: \(totalCorrectAnswers)"
+        lbWrong.text = "Perguntas Incorretas: \(totalAnswers - totalCorrectAnswers)"
+        
+        let score = totalCorrectAnswers*100/totalAnswers
+        
+        lbScore.text = "\(score)%"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
     @IBAction func close(_ sender: UIButton) {
+            dismiss(animated: true, completion: nil)
     }
     
 }
